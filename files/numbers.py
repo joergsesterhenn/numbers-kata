8de3107b263e34cc834f9cc6b41dd5b94f994784
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-03 17:16:33 +0000

    15 ran tests, predicted green, got red

--- files/numbers.py
+++ files/numbers.py
@@ -1,4 +1,6 @@
 class Numbers:
-    digits ['zero','one','two','three','four','five','six','seven','eight','nine']
+    
+    digits = ['zero','one','two','three','four','five','six','seven','eight','nine']
+    
     def to_number(self, number):
         return self.digits[number]
