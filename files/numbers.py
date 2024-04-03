f41b7b26ae085c776347fea2409aced0717c7aa9
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-03 17:46:04 +0000

    24 ran tests, predicted green, got green

--- files/numbers.py
+++ files/numbers.py
@@ -1,6 +1,13 @@
 class Numbers:
     
     digits = ['zero','one','two','three','four','five','six','seven','eight','nine']
+    # everytime we hit zero there is a new special number until we get to hundreds so we need the other numbers as separate array
+    units = digits[1:]
+    # all teens are special !
+    teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    # here are the special numbers up to 100
+    tens = ['twenty','thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    
     
     def to_number(self, number):
         if number < 10:
