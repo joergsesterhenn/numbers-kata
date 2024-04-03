c86db992143bed49ff61a084e4ef2737e36ec282
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-03 16:30:25 +0000

    0 kata creation

---
+++ files/test_hiker.py
@@ -0,0 +1,28 @@
+from hiker import global_answer, Hiker
+import unittest
+from approvaltests.approvals import verify
+from approvaltests.reporters.report_on_cyber_dojo import ReportOnCyberDojo
+from approvaltests import set_default_reporter
+
+
+class HikerTest(unittest.TestCase):
+    def setUp(self):
+        set_default_reporter(ReportOnCyberDojo())
+
+    # if you want to change the expected result, 
+    # move HikerTest.test_global.recieved.txt
+    # to   HikerTest.test_global.approved.txt
+    # to view the differences, 
+    # open HikerTest.test_global.diff
+
+    def test_global(self):
+        result = str(global_answer())
+        verify(result)
+
+    def test_instance(self):
+        result = str(Hiker().instance_answer())
+        verify(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
