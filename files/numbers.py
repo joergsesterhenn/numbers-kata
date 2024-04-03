e6d0fb671a74d09df38c85f7f714e1953cb68055
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-03 17:20:46 +0000

    18 ran tests, predicted red, got red

--- files/numbers.py
+++ files/numbers.py
@@ -3,4 +3,7 @@ class Numbers:
     digits = ['zero','one','two','three','four','five','six','seven','eight','nine']
     
     def to_number(self, number):
-        return self.digits[number]
+        if number < 10:
+            return self.digits[number]
+        else:
+            return "ten"
