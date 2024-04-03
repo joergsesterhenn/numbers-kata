d7085887abdfc3595604ffb92a331ca10214965a
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-03 18:06:29 +0000

    38 ran tests, predicted green, got green

--- files/numbers.py
+++ files/numbers.py
@@ -1,8 +1,6 @@
 class Numbers:
     
     digits = ['zero','one','two','three','four','five','six','seven','eight','nine']
-    # everytime we hit zero there is a new special number until we get to hundreds so we need the other numbers as separate array
-    units = digits[1:]
     # all teens are special !
     teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
     # here are the special numbers up to 100
@@ -15,7 +13,7 @@ class Numbers:
         elif self.is_teen(number):
             return self.teens[int(str(number)[1])]
         else:
-            return self.tens[int(str(number)[0])] + ' ' + self.units[int(str(number)[1])]
+            return self.tens[int(str(number)[0])] + ' ' + self.digits[int(str(number)[1])]
         
     def is_digit(self, number):
         return number < 10
