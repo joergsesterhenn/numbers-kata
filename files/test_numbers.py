92ac38691783153581a269dcc041f2b18c2b8f7c
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-03 16:38:56 +0000

    4 ran tests, predicted green, got green

--- files/test_numbers.py
+++ files/test_numbers.py
@@ -5,6 +5,8 @@ from approvaltests.reporters.report_on_cyber_dojo import ReportOnCyberDojo
 from approvaltests import set_default_reporter
 
 
+
+
 class NumbersTest(unittest.TestCase):
     def setUp(self):
         set_default_reporter(ReportOnCyberDojo())
@@ -14,6 +16,21 @@ class NumbersTest(unittest.TestCase):
     # to   HikerTest.test_global.approved.txt
     # to view the differences, 
     # open HikerTest.test_global.diff
+    
+    # testplan:
+    #
+    # test number one
+    # test single digit
+    # test ten
+    # test two digits
+    # 99 --> ninety nine
+    # test one hundred
+    # 300 --> three hundred
+    # 310 --> three hundred and ten
+    # 1501 --> one thousand, five hundred and one
+    # 12609 --> twelve thousand, six hundred and nine
+    # 512607 --> five hundred and twelve thousand, six hundred and seven
+    # 43112603 --> forty three million, one hundred and twelve thousand, six hundred and three
 
     def test_global(self):
         result = str(global_answer())
