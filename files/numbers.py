b321feae4162214c51191ee6ff4094c9fe4a1257
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-03 18:03:48 +0000

    36 ran tests, predicted green, got red

--- files/numbers.py
+++ files/numbers.py
@@ -6,7 +6,7 @@ class Numbers:
     # all teens are special !
     teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
     # here are the special numbers up to 100
-    tens = ['ten', 'twenty','thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    tens = ['zero', 'ten', 'twenty','thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
     
     
     def to_number(self, number):
