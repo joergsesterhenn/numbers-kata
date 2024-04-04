3da686606bd6407fb5d0c83d2b6274ff539542e0
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-04 13:36:44 +0000

    126 ran tests, predicted green, got green

--- files/numbers.py
+++ files/numbers.py
@@ -35,11 +35,11 @@ class Numbers:
     def to_number(self, number: int):
         array_of_numbers_by_order = self.disect_number(number)
         order_of_number = (len(str(number))-1)//3
-        print (number, array_of_numbers_by_order)
+        #print (number, array_of_numbers_by_order)
         number_string=''
         for order, number_part in enumerate(array_of_numbers_by_order):
             number_string = number_string + (self.get_number_by_order(number_part, order))
-            print (number, order, number_part, self.orders[order], number_string)
+            #print (number, order, number_part, self.orders[order], number_string)
             if order_of_number-order >0:
                 number_string = number_string + ' ' + self.orders[order+1] + ', ' 
         return number_string
