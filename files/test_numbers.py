538391f00f7d0ec87008fecfe524d423589da9ae
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-04 13:18:25 +0000

    115 ran tests, predicted red, got green

--- files/test_numbers.py
+++ files/test_numbers.py
@@ -31,45 +31,45 @@ class NumbersTest(unittest.TestCase):
         result = str(Numbers().to_number(1))
         verify(result)
     
-    def test_single_digits(self):
-        result =  [Numbers().to_number(number) for number in range(1,10)]
-        verify(result)
-
-    def test_ten(self):
-        result = str(Numbers().to_number(10))
-        verify(result)
-        
-    def test_ninety_nine(self):
-        result = str(Numbers().to_number(99))
-        verify(result)
-
-    def test_every_two_digit_number(self):
-        result = [Numbers().to_number(number) for number in range(10,100)]
-        verify(result)
-
-    def test_one_hundred(self):
-        result = Numbers().to_number(100)
-        verify(result)
-        
-    def test_three_hundred(self):
-        result = Numbers().to_number(300)
-        verify(result)
-        
-    def test_three_hundred_ten(self):
-        result = Numbers().to_number(310)
-        verify(result)
-        
-    def test_every_three_digit_number(self):
-        result = [Numbers().to_number(number) for number in range(100,1000)]
-        verify(result)
-        
-    def test_one_thousand_five_hundred_and_one(self):
-        result = Numbers().to_number(1501)
-        verify(result)
-        
+#   def test_single_digits(self):
+#       result =  [Numbers().to_number(number) for number in range(1,10)]
+#       verify(result)
+#
+#   def test_ten(self):
+#       result = str(Numbers().to_number(10))
+#       verify(result)
+#       
+#   def test_ninety_nine(self):
+#       result = str(Numbers().to_number(99))
+#       verify(result)
+#
+#   def test_every_two_digit_number(self):
+#       result = [Numbers().to_number(number) for number in range(10,100)]
+#       verify(result)
+#
+#   def test_one_hundred(self):
+#       result = Numbers().to_number(100)
+#       verify(result)
+#       
+#   def test_three_hundred(self):
+#       result = Numbers().to_number(300)
+#       verify(result)
+#       
+#   def test_three_hundred_ten(self):
+#       result = Numbers().to_number(310)
+#       verify(result)
+#       
+#   def test_every_three_digit_number(self):
+#       result = [Numbers().to_number(number) for number in range(100,1000)]
+#       verify(result)
+#       
+#   def test_one_thousand_five_hundred_and_one(self):
+#       result = Numbers().to_number(1501)
+#       verify(result)
+#       
     def test_disecting_to_order(self):
         result = Numbers().disect_number(1501)
         verify(result)
-        
+       
 if __name__ == "__main__":
     unittest.main()
