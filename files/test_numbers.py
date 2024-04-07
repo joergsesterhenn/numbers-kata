2386cb4435372afdbab9ed400b4c09e6c0798422
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-07 19:04:44 +0000

    173 ran tests, predicted green, got green

--- files/test_numbers.py
+++ files/test_numbers.py
@@ -84,7 +84,7 @@ class NumbersTest(unittest.TestCase):
         verify(result)
         
     def test_one_million(self):
-        result = Numbers().to_number(1001001)
+        result = Numbers().to_number(1000000)
         verify(result)
        
 if __name__ == "__main__":
