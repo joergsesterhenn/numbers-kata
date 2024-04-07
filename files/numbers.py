b6b248f2afe5a3c239f93274a9d919528cc8a95f
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-07 20:11:02 +0000

    200 ran tests, predicted green, got green

--- files/numbers.py
+++ files/numbers.py
@@ -88,9 +88,9 @@ class Numbers:
     def disect_number(self, number: int):
         order_of_number = (len(str(number))-1)//3
         length_of_number=len(str(number))
-        #print('number: ',number)
-        #print('order: ',order_of_number)
-        #print('length: ',length_of_number)
+        print('number: ',number)
+        print('order: ',order_of_number)
+        print('length: ',length_of_number)
         array_of_ordered_numbers = []
         for order in reversed((range(-1*(order_of_number+1),0))):
             start=max(3*order,-1*length_of_number)
