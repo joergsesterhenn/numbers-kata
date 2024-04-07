bb6f9c4ba2ecfb15e426db08a078b0c4abc26f89
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-07 21:24:51 +0000

    218 ran tests, predicted red, got red

--- files/test_numbers.py
+++ files/test_numbers.py
@@ -27,9 +27,9 @@ class NumbersTest(unittest.TestCase):
     # x 512607 --> five hundred and twelve thousand, six hundred and seven
     # x 43112603 --> forty three million, one hundred and twelve thousand, six hundred and three
       
-    def test_number_one(self):
-        result = str(Numbers().to_number(1))
-        verify(result)
+#    def test_number_one(self):
+#        result = str(Numbers().to_number(1))
+#        verify(result)
     
     def test_single_digits(self):
         result =  [Numbers().to_number(number) for number in range(1,10)]
@@ -42,10 +42,10 @@ class NumbersTest(unittest.TestCase):
     def test_ninety_nine(self):
         result = str(Numbers().to_number(99))
         verify(result)
- 
-    def test_every_two_digit_number(self):
-        result = "\n".join([Numbers().to_number(number) for number in range(10,100)])
-        verify(result)
+
+#    def test_every_two_digit_number(self):
+#        result = "\n".join([Numbers().to_number(number) for number in range(10,100)])
+#        verify(result)
  
     def test_one_hundred(self):
         result = Numbers().to_number(100)
@@ -59,9 +59,9 @@ class NumbersTest(unittest.TestCase):
         result = Numbers().to_number(310)
         verify(result)
         
-    def test_every_three_digit_number(self):
-        result = "\n".join([Numbers().to_number(number) for number in range(100,1000)])
-        verify(result)
+#    def test_every_three_digit_number(self):
+#        result = "\n".join([Numbers().to_number(number) for number in range(100,1000)])
+#        verify(result)
         
     def test_one_thousand_five_hundred_and_one(self):
         result = Numbers().to_number(1501)
@@ -75,9 +75,9 @@ class NumbersTest(unittest.TestCase):
         result = Numbers().to_number(12609)
         verify(result)
     
-    def test_five_hundred_and_twelve_thousand_six_hundred_and_seven(self):
-        result = Numbers().to_number(512607)
-        verify(result)
+#    def test_five_hundred_and_twelve_thousand_six_hundred_and_seven(self):
+#        result = Numbers().to_number(512607)
+#        verify(result)
     
     def test_forty_three_million(self):
         result = Numbers().to_number(43112603)
@@ -87,9 +87,9 @@ class NumbersTest(unittest.TestCase):
         result = Numbers().to_number(1000000)
         verify(result)
         
-    def test_sample_in_millions(self):
-        result = "\n".join([Numbers().to_number(number) for number in range(1000000,10000000,154321)])
-        verify(result)
+#    def test_sample_in_millions(self):
+#        result = "\n".join([Numbers().to_number(number) for number in range(1000000,10000000,154321)])
+#        verify(result)
        
     def test_a_really_long_one(self):
         result = Numbers().to_number(1050907000407750600030640022401)
