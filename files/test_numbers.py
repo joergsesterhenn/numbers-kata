70ab3bd954d8620e848b58007b6295c2e09fd6a8
v8wNWc <v8wNWc@cyber-dojo.org>
2024-04-07 20:28:26 +0000

    211 ran tests, predicted green, got green

--- files/test_numbers.py
+++ files/test_numbers.py
@@ -87,9 +87,9 @@ class NumbersTest(unittest.TestCase):
         result = Numbers().to_number(1000000)
         verify(result)
         
-   # def test_sample_in_millions(self):
-   #     result = "\n".join([Numbers().to_number(number) for number in range(1000000,10000000,154321)])
-   #     verify(result)
+    def test_sample_in_millions(self):
+        result = "\n".join([Numbers().to_number(number) for number in range(1000000,10000000,154321)])
+        verify(result)
        
 if __name__ == "__main__":
     unittest.main()
